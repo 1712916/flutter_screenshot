a7e496f844e65985a1acc02f97baa600effc88db
1712916 <kieuphong2840@gmail.com>
2023-12-19 14:41:44 +0700

    create another page to test screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,36 @@ import 'package:flutter/material.dart';
 
 import 'screenshot_listener/screenshot_listener.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        AnotherPage.otherPageRoute: (context) => const AnotherPage(),
+      },
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final ScreenShotListener _listener = ScreenShotListener.getInstance();
 
   String? text;
@@ -34,18 +56,44 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Detect Screenshot Callback Example'),
-        ),
-        body: Center(
-          child: text == null
-              ? const Text(
-                  "Ready",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )
-              : Image.file(File(text!)),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detect Screenshot Callback Example'),
+      ),
+      body: Center(
+        child: text == null
+            ? const Text(
+                "Ready",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )
+            : Image.file(File(text!)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed(AnotherPage.otherPageRoute);
+        },
+        child: const Icon(Icons.navigate_next_outlined),
+      ),
+    );
+  }
+}
+
+class AnotherPage extends StatelessWidget {
+  static const String otherPageRoute = "other";
+
+  const AnotherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Another Page"),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.orange,
+          width: 300,
+          height: 300,
         ),
       ),
     );
